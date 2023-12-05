ffabc43d9ff3b3184c953fb6b15aea57fd50c8f0
italo.sorbara <italo.sorbara>
2023-12-05 23:54:38 +0100

    update

---
+++ TestUnitTvMaze/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestUnitTvMaze")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestUnitTvMaze")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5783544a-ae35-497c-bc04-098efd9e40a1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestUnitTvMaze/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MongoDB.Driver;
+using System;
+//using System.Web.Http;
+using Test.TvMaze.Controllers;
+using TvMaze.Project.Models;
+using TvMaze.Project.Services;
+
+namespace TestUnitTvMaze
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private readonly TvMazeController _controller;
+        private readonly IJwtService _jwtService;
+        private readonly IMongoCollection<Show> _collection;
+
+        public UnitTest1()
+        {
+            //_jwtService = new JwtService();
+            var settings = MongoClientSettings.FromUrl(new MongoUrl("mongodb://localhost:27017"));
+            var client = new MongoClient(settings);
+            var databases = client.GetDatabase("TvMazeDb");
+            _collection = databases.GetCollection<Show>("shows");
+            //_controller = new TvMazeController(_jwtService, _collection.Database);
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+             // Arrange
+        int showId = 1;
+        // Act
+        //IHttpActionResult result = _controller.GetShowById(showId);
+        ///// Assert
+        //Assert.IsType<OkNegotiatedContentResult<Show>>(result);
+        }
+    }
+}
